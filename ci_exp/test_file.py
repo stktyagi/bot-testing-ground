255e5eac9753718246775bdae3908b42518f6fc1
stktyagi <stktyagi2810@gmail.com>
2026-02-17 22:32:37 +0530

    fork-trigger

--- ci_exp/test_file.py
+++ ci_exp/test_file.py
@@ -3,7 +3,7 @@ from file import add
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(5, 3), 8)
+        self.assertEqual(add(5, 3), 1)
 
 if __name__ == '__main__':
     unittest.main()
